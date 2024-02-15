9545a0503f45acf547f696d31300fbf3c525893c
logoutbye <muhammadbilal03478@gmail.com>
2024-02-15 10:13:54 +0500

    data folder added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:liveproject/utilis/Routes/Named_Routes.dart';
 import 'package:liveproject/utilis/Routes/routess.dart';
-
-
 import 'package:liveproject/utilis/Themesdata/theme_light.dart';
-import 'package:liveproject/view/driver_side_screens/deriver_info/deriver_basic_info.dart';
-import 'package:liveproject/view/driver_side_screens/ride_detail_screens/bottom_navigation.dart';
-import 'package:liveproject/view/driver_side_screens/vehicle_info/vehicle_basic_info.dart';
-import 'package:liveproject/view/driver_side_screens/vehicle_info/vehicle_info_detail.dart';
+
 
 
 
